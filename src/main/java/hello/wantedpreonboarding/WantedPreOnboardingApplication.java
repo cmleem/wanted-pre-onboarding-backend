10980ab349650d2b6adbee6325b2097129cbbdc9
cmleem <coals722@naver.com>
2024-07-24 16:14:07 +0900

    Initial Commit

---
+++ src/main/java/hello/wantedpreonboarding/WantedPreOnboardingApplication.java
@@ -0,0 +1,13 @@
+package hello.wantedpreonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedPreOnboardingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/wantedpreonboarding/WantedPreOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.wantedpreonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreOnboardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
